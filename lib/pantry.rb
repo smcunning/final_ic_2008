4c7be34d6bbf0109073b0aa7ffb9a143a48b4536
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-23 09:14:28 -0600

    Add method for stock_check

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -3,4 +3,12 @@ class Pantry
   def initialize
     @stock = {}
   end
+
+  def stock_check(ingredient_obj)
+    if @stock.include?(ingredient_obj)
+      @stock[ingredient_obj]
+    else
+      0
+    end 
+  end
 end
