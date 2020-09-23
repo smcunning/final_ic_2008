edeaaf2bda1d08dbdd27d21c6da93474019d2b06
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-23 09:33:04 -0600

    Add test for adding recipes

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -12,4 +12,18 @@ class CookBookTest < Minitest::Test
     assert_instance_of CookBook, cookbook
     assert_equal [], cookbook.recipes
   end
+
+  def test_it_can_add_recipes
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end
