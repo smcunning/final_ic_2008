74ba65061430064b6ab1072e39dd88e344660744
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-23 10:02:12 -0600

    Update test to remove unncessary setup data

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -36,13 +36,6 @@ class PantryTest < Minitest::Test
     recipe1.add_ingredient(ingredient1, 2)
     recipe1.add_ingredient(ingredient2, 8)
 
-    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
-    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
-    recipe2 = Recipe.new("Cheese Burger")
-    recipe2.add_ingredient(ingredient1, 2)
-    recipe2.add_ingredient(ingredient3, 4)
-    recipe2.add_ingredient(ingredient4, 1)
-
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
 
