43fde06770290a79c667fbf80a883f9897a757f4
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-23 09:43:11 -0600

    Add test to list all ingredients

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -26,4 +26,25 @@ class CookBookTest < Minitest::Test
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
+
+  def test_it_can_list_all_ingredients
+    pantry = Pantry.new
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], cookbook.ingredients
+  end
 end
