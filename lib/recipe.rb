009e5a8247b00251cb2e0d50880f66c82e4929f1
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-23 10:13:32 -0600

    Style refactor attr_reader

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe
-  attr_reader :name, :ingredients_required
+  attr_reader :name,
+              :ingredients_required
   def initialize(name)
     @name = name
     @ingredients_required = {}
