d1d03193280a04f634a302f40925af8cbc2947d5
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-23 09:19:03 -0600

    Create Recipe class test and test with attributes

---
+++ test/recipe_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/ingredient"
+require "./lib/pantry"
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def test_it_exists_with_attributes
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    assert_instance_of Recipe, recipe1
+    assert_equal "Mac and Cheese", recipe1.name
+    assert_equal ({}), recipe1.ingredients_required
+  end 
+end
